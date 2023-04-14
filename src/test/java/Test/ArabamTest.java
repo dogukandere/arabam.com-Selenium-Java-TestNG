d80db1fe028c9047a1a8716e36249106159b2ab5
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-04-14 12:37:19 +0300

    Update ArabamTest.java

--- src/test/java/Test/ArabamTest.java
+++ src/test/java/Test/ArabamTest.java
@@ -1,2 +1,29 @@
-package Test;public class ArabamTest {
+package Test;
+
+import Page.AllAdsPage;
+import Page.ZeroKmCarPage;
+import Utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class ArabamTest extends TestBase {
+     ZeroKmCarPage zeroKmCarPage;
+     AllAdsPage allAdsPage;
+    @Test(priority = 1 , description = "Search a car on the zero km car page")
+    public void test01(){
+        zeroKmCarPage = new ZeroKmCarPage(driver);
+        zeroKmCarPage.navigateToUrl();
+        zeroKmCarPage.goToZeroKmCarPage();
+        zeroKmCarPage.selectBrand();
+        zeroKmCarPage.selecetModel();
+        zeroKmCarPage.searhCar();
+        zeroKmCarPage.verify();
+    }
+    @Test(priority = 2 , description = "Verify the all ads page")
+    public void test02(){
+        allAdsPage = new AllAdsPage(driver);
+        zeroKmCarPage.navigateToUrl();
+        allAdsPage.goToAllAds();
+        allAdsPage.verifyClickable();
+        allAdsPage.verifyLogoLinkTitle();
+    }
 }
