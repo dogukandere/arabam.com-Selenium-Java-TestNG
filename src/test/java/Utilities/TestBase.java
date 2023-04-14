95feb303917b11c26292cc32d4b46065e4826f69
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-04-14 12:39:49 +0300

    Update TestBase.java

--- src/test/java/Utilities/TestBase.java
+++ src/test/java/Utilities/TestBase.java
@@ -1,2 +1,24 @@
-package Utilities;public class TestBase {
+package Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public abstract class TestBase {
+    protected WebDriver driver;
+    @BeforeClass
+    public void Before(){
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @AfterClass
+    public void After(){
+        driver.quit();
+    }
 }
