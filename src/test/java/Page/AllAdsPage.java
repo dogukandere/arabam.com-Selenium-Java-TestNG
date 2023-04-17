30b817c72c76f7b0d0e4efdde8a1d1b96d5f36a2
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-04-17 13:20:00 +0300

    Update AllAdsPage.java

--- src/test/java/Page/AllAdsPage.java
+++ src/test/java/Page/AllAdsPage.java
@@ -1,35 +1,24 @@
-package Page;
+package Pages;
 
 import Utilities.ReusableMethods;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
-public class AllAds extends ReusableMethods {
+public class AllAdsPage extends ReusableMethods {
     WebDriver driver;
 
-    public AllAds(WebDriver driver) {
+    public AllAdsPage(WebDriver driver) {
         super(driver);
         this.driver = driver;
         PageFactory.initElements(driver,this);
     }
 
-    @FindBy(xpath = "//span[contains(text(),'Tüm İlanlar')]")
-    private WebElement allAdsElement;
-
-    @FindBy(xpath = "//a[@class='fav-menu-button']")
-    private WebElement favAdsButton;
-
-    @FindBy(xpath = "//button[@id='favSearchButton']")
-    private WebElement favSearchButton;
-
-    @FindBy(xpath = "//button[@id='compareButton']")
-    private WebElement compareAdsButton;
-
-    @FindBy(xpath = "//img[@alt='arabam.com']")
-    private WebElement logo;
+    private final String allAdsElement =  "//span[contains(text(),'Tüm İlanlar')]";
+    private final String favAdsButton = "//a[@class='fav-menu-button']";
+    private final String favSearchButton = "//button[@id='favSearchButton']";
+    private final String compareAdsButton = "//button[@id='compareButton']";
+    private final String logo =  "//img[@alt='arabam.com']";
 
     public void goToAllAds(){
         click(allAdsElement);
@@ -44,5 +33,6 @@ public class AllAds extends ReusableMethods {
         isDisplayed(logo);
         Assert.assertEquals("https://www.arabam.com/ikinci-el",driver.getCurrentUrl());
         Assert.assertEquals("İkinci El Araba Fiyatları, Satılık 2. El Araba Modelleri arabam.com'da!",driver.getTitle());
+        waitFor(3);
     }
 }
