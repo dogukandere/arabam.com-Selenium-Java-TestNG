f1d11a0327d29c54735073bb35968b5f7d40b926
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-04-17 13:18:25 +0300

    Update ReusableMethods.java

--- src/test/java/Utilities/ReusableMethods.java
+++ src/test/java/Utilities/ReusableMethods.java
@@ -1,148 +1,25 @@
 package Utilities;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
-import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
-import static java.lang.Math.abs;
 
 public class ReusableMethods {
     WebDriver driver;
     WebDriverWait wait;
 
     public ReusableMethods(WebDriverWait wait) {
-
         this.wait = wait;
     }
 
     public ReusableMethods(WebDriver driver) {
-
         this.driver = driver;
-    }
-
-    public void rightClick(WebElement element){
-        Actions action = new Actions(driver);
-        action.contextClick(element).perform(); //mouse sağ tık
-    }
-
-    public void doubleClick(WebElement element){
-        Actions action = new Actions(driver);
-        action.doubleClick(element).perform(); //mouse sağ tık
-    }
-
-    public void screenShot() throws IOException {
-        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(scrFile, new File("/Users/dogukandere/Downloads/batch30-POM-main/src/SS/dddd.jpg"));
-    }
-
-    public ReusableMethods waitFor(int sec) {
-        try {
-            Thread.sleep(sec * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return this;
-    }
-
-    public void scrollDown(int pixels) {
-        JavascriptExecutor js = (JavascriptExecutor)driver;
-        js.executeScript("window.scrollBy(0, " + abs(pixels) + ");", "");
-    }
-
-    public void scrollIntoView(WebElement element) {
-        JavascriptExecutor js = (JavascriptExecutor)driver;
-        js.executeScript("arguments[0].scrollIntoView(false);", element);
-    }
-
-    public void hover(WebElement element) {
-        Actions actions = new Actions(driver);
-        actions.moveToElement(element).perform();
-    }
-
-    public void hoverOverMenu(List<WebElement> list){
-        Actions action = new Actions(driver);
-        for(WebElement menüler:list ){
-            action.moveToElement(menüler).perform();
-            waitFor(1);
-            System.out.println(menüler.getText());
-        }
-    }
-
-    public void getPageUrl() {
-        System.out.println(driver.getCurrentUrl());
-    }
-
-    public void getPageTitle() {
-        System.out.println(driver.getTitle());
-    }
-
-    public void navigateRefresh(){driver.navigate().refresh(); }
-
-    public void navigateBack(){driver.navigate().back(); }
-
-    public void navigateForward(){driver.navigate().forward(); }
-
-    public void deleteCookies(){driver.manage().deleteAllCookies(); }
-
-    public void findAllCookies(){
-        Set <Cookie> cookies = driver.manage().getCookies();
-        for(Cookie cookie:cookies){
-            System.out.println(cookie.getName()+":"+cookie.getValue());
-        }
-        System.out.println("Total cookies:"+cookies.size());
-    }
-
-    public void switchToTab(){
-        Set<String> tabs = driver.getWindowHandles(); //Used to switch to a new tab that opens
-        for(String actual: tabs) {
-            driver.switchTo().window(actual);
-        }
-    }
-
-    public ReusableMethods switchToWindow2(String targetTitle) {
-        String origin = driver.getWindowHandle();
-        for (String handle : driver.getWindowHandles()) {
-            driver.switchTo().window(handle);
-            if (driver.getTitle().equals(targetTitle)) {
-                return this;
-            }
-        }
-        driver.switchTo().window(origin);
-        return null;
-    }
-
-    public void dropDownSelectByValue(WebElement element,String value){
-        Select Value = new Select(element);
-        Value.selectByValue(value);
-    }
-
-    public void dropDownSelectByIndex(WebElement element,int value){
-        Select index = new Select(element);
-        index.selectByIndex(value);
-    }
-
-    public void dropDownSelectByText(WebElement element,String value){
-        Select VisibleText = new Select (element);
-        VisibleText.selectByVisibleText(value);
-    }
-
-    public void getElementSize(WebElement element){
-
-        System.out.println(element.getSize());
-    }
-
-    public void getElementLocation(WebElement element) {
-
-        System.out.println(element.getLocation());
     }
 
     public void waitUntilElementIsVisible(WebElement element , int time){
@@ -155,64 +32,47 @@ public class ReusableMethods {
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    public void dragToElement(WebElement element, WebElement element2){
-        Actions action = new Actions(driver);
-        action.dragAndDrop(element,element2).build().perform();
+    public void click(String xpath){
+        WebElement element = driver.findElement(By.xpath(xpath));
+        waitUntilElementIsVisible(element,10);
+        element.click();
     }
 
-    public void dragThisCoordinate(WebElement element, int x, int y){
-        Actions action = new Actions(driver);
-        action.dragAndDropBy(element,x,y).perform();
-    }
-
-    public ReusableMethods getElementsText(List<WebElement> list) {
-        List<String> elemTexts = new ArrayList<>();
-        for (WebElement el : list) {
-            if (!el.getText().isEmpty()) {
-                System.out.println(elemTexts.add(el.getText()));
-            }
-        }
-        return this;
-    }
-    public void elementContains(WebElement element, String word){
-        String x= element.getText();
-        Assert.assertTrue(word.contains(x));
-    }
-
-    public void findAllLinks(){
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println("Total link number "+ links.size());
-        for(int i=0;i<links.size();i++)
-        {
-            System.out.println("Link url: "+links.get(i).getAttribute("href"));
-            System.out.println("Link name:"+links.get(i).getText());
-        }
-    }
-
-    public void copyPasteText(WebElement element, WebElement element2){
-        element.sendKeys(Keys.COMMAND + "a");
-        element.sendKeys(Keys.COMMAND + "c");
-        element2.sendKeys(Keys.COMMAND + "v");
-    }
-
-    public void isDisplayed(WebElement element){
+    public void isDisplayed(String xpath){
+        WebElement element = driver.findElement(By.xpath(xpath));
         waitUntilElementIsVisible(element,10);
         Assert.assertTrue(element.isDisplayed());
     }
 
-    public void isClickable(WebElement element){
-        waitUntilElementIsVisible(element,10);
+    public void isClickable(String xpath){
+        WebElement element = driver.findElement(By.xpath(xpath));
+        waitUntilElementIsClickable(element,10);
         Assert.assertTrue(element.isEnabled());
     }
 
-    public void click(WebElement element){
-        waitUntilElementIsClickable(element,10);
-        element.click();
-    }
-
-    public void SendKeys(WebElement element, String value){
+    public void sendKeys(String xpath, String value){
+        WebElement element = driver.findElement(By.xpath(xpath));
         waitUntilElementIsVisible(element,10);
         element.sendKeys(value);
     }
 
+    public ReusableMethods waitFor(int sec) {
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public void dropDownSelectByText(String xpath,String value){
+        WebElement element = driver.findElement(By.xpath(xpath));
+        Select VisibleText = new Select (element);
+        VisibleText.selectByVisibleText(value);
+    }
+
+    public void textAssert(String xpath, String expected){
+        WebElement element = driver.findElement(By.xpath(xpath));
+        Assert.assertEquals(element.getText(),expected);
+    }
 }
